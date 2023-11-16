4997ab5333bbd33fe1bdb89d396eba8abdccf2f7
boss_ <boss_@DESKTOP-LKQV3S7>
2023-11-16 15:42:53 +0200

    Add project files.

---
+++ Moldovan_Raul_Lab2/Models/Author.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Moldovan_Raul_Lab2.Models
+{
+    public class Author
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "FirstName")]
+        public string? FirstName { get; set; }
+
+        [Display(Name = "LastName")]
+        public string? LastName { get; set; }
+
+        public ICollection<Book>? Books { get; set; }
+        public int AuthorId { get; set; }
+        public string Name { get; set; }
+    }
+}
